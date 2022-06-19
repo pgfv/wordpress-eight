b53b137299d9a1091293da4095bb700f6b6f511c
kurtkungz <kurtkungz@gmail.com>
2022-06-19 21:57:39 +0700

    add `mt_author_box` support for plugin

--- single.php
+++ single.php
@@ -1,5 +1,13 @@
 <?php get_header(); ?>
 <section class="prose max-w-none main-content">
     <?php the_content(); ?>
+
+    <?php if ( function_exists( 'mt_author_box' ) ) {
+    mt_author_box( $post->post_author, array(
+        'theme'              => 'tabbed',
+        'profileAvatarShape' => 'round',
+        'showWebsite'        => true,
+    ) );
+    } ?>
 </section>
 <?php get_footer(); ?>
