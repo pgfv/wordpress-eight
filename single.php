24d13d319966b6f82e7e33ea8ebf43d3327a956c
kurtkungz <kurtkungz@gmail.com>
2022-06-19 22:07:37 +0700

    test `mt_profile_img` function

--- single.php
+++ single.php
@@ -2,11 +2,19 @@
 <section class="prose max-w-none main-content">
     <?php the_content(); ?>
 
+    <?php
+    $author_id = $post->post_author;
+    mt_profile_img( $author_id, array(
+        'size' => 'thumbnail',
+        'attr' => array( 'alt' => 'Alternative Text' ),
+        'echo' => true )
+    );
+    ?>
+
     <?php if ( function_exists( 'mt_author_box' ) ) {
     mt_author_box( $post->post_author, array(
         'theme'              => 'tabbed',
         'profileAvatarShape' => 'round',
-        'showWebsite'        => true,
     ) );
     } ?>
 </section>
